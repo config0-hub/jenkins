0c8d69d9bd0c5fd236752015ff2e00db5a358eda
gary <gary>
2024-12-17 11:31:49 +0800

    rendered and updated 12-17-2024-11-31

---
+++ stacks/_config0_configs/jenkins_on_docker/_documentation/README.md
@@ -0,0 +1,23 @@
+**Description**
+
+  - This stack sets up Jenkins with Docker on the specified hostname.
+
+**Infrastructure**
+
+To proceed with the setup, please ensure that 
+  - the hostname has been created and registered with Config0. 
+  - the SSH key name has been uploaded and registered in Config0 beforehand.
+
+**Required**
+
+| argument      | description                           | var type | default      |
+| ------------- |---------------------------------------| -------- | ------------ |
+| hostname   | the hostname to install jenkins       | string   | None         |
+| ssh_key_name   | the ssh_key_name to log into hostname | string   | None         |
+
+**Optional**
+
+| argument           | description                                                                       | var type | default |
+| ------------- |-----------------------------------------------------------------------------------| -------- |---------|
+| publish_private_key   | if set true, the private key in base64 hash will show on the Config0 UI           | string   | None    |
+| ansible_docker_image   | overide the default the docker runtime to execute ansible used to install jenkins | string   | config0/ansible-run-env        |
---
+++ stacks/_config0_configs/jenkins_on_docker/_main/run.py
@@ -0,0 +1,102 @@
+def _get_public_ip(stack):
+
+    _lookup = {"must_exists": True,
+               "must_be_one": True,
+               "resource_type": "server",
+               "hostname": stack.hostname}
+
+    return list(stack.get_resource(**_lookup))[0]["public_ip"]
+
+
+def _get_private_key_hash(stack):
+
+    _lookup = {"must_exists": True,
+               "resource_type": "ssh_key_pair",
+               "name": stack.ssh_key_name}
+
+    try:
+        private_key = stack.get_resource(decrypt=True,
+                                         **_lookup)[0]["private_key"]
+    except:
+        _lookup["resource_type"] = "ssh_public_key"
+        private_key = stack.get_resource(decrypt=True,
+                                         **_lookup)[0]["private_key"]
+
+    return stack.b64_encode(private_key)
+
+def run(stackargs):
+
+    import json
+
+    # instantiate authoring stack
+    stack = newStack(stackargs)
+
+    # Add default variables
+    stack.parse.add_required(key="hostname")
+
+    stack.parse.add_required(key="ssh_key_name")
+
+    stack.parse.add_required(key="publish_private_key",
+                             default="null")
+
+    stack.parse.add_optional(key="ansible_docker_image",
+                             default="config0/ansible-run-env")
+
+    # Add execgroup
+    stack.add_execgroup("config0-publish:::jenkins::on_docker")
+    stack.add_substack("config0-publish:::config0_core::publish_host_file")
+
+    # Initialize
+    stack.init_variables()
+    stack.init_execgroups()
+    stack.init_substacks()
+
+    public_ip = _get_public_ip(stack)
+    _private_key_hash = _get_private_key_hash(stack)
+
+    # generate stateful id
+    stateful_id = stack.random_id(size=10)
+
+    env_vars = {"STATEFUL_ID": stateful_id,
+                "DOCKER_IMAGE": stack.ansible_docker_image,
+                "ANSIBLE_DIR": "var/tmp/ansible",
+                "ANS_VAR_private_key": _private_key_hash,  # expects base64 string
+                "ANS_VAR_hosts": stack.b64_encode(json.dumps({"all": [public_ip]})),
+                "ANS_VAR_exec_ymls": "install.yml",
+                "ANSIBLE_EXEC_YMLS": "install.yml"}
+
+    human_description= "Install Jenkins for Ansible"
+    inputargs = {"display": True,
+                 "human_description": human_description,
+                 "env_vars": json.dumps(env_vars.copy()),
+                 "stateful_id": stateful_id,
+                 "automation_phase": "infrastructure",
+                 "hostname": stack.hostname}
+
+    stack.on_docker.insert(**inputargs)
+
+    # publish variables
+    _publish_vars = {"hostname": stack.hostname,
+                     "jenkins_ipaddress": public_ip,
+                     "jenkins_url": "https://{}".format(public_ip),
+                     "jenkins_user": "admin"}
+
+    if stack.publish_private_key:
+        _publish_vars["private_key_b64"] = _private_key_hash
+
+    stack.publish(_publish_vars)
+
+    # fetch and publish jenkins admin password
+    arguments = {"remote_file": "/var/lib/jenkins/secrets/initialAdminPassword",
+                 "key": "jenkins_password",
+                 "hostname": stack.hostname,
+                 "ssh_key_name": stack.ssh_key_name}
+
+    human_description = "Publish jenkins admin init password"
+    inputargs = {"arguments": arguments,
+                 "automation_phase": "infrastructure",
+                 "human_description": human_description}
+
+    stack.publish_host_file.insert(display=True, **inputargs)
+
+    return stack.get_results()
